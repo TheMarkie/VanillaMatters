f9dfcf77ae66d9276f7d5f573a08cb09b5131be9
TheMarkie <themarkie23@gmail.com>
2018-04-22 19:15:05 +0700

    added launcher files to master
    - VMN branch will be pruned until VM is somewhat final.

---
+++ VanillaMattersLauncher/Launcher.h
@@ -0,0 +1,9 @@
+#pragma warning( disable : 4201 )
+#define STRICT
+#include <windows.h>
+#include <commctrl.h>
+#include <shlobj.h>
+#include "Engine.h"
+#include "UnRender.h"
+#include "Window.h"
+#include "resource.h"
