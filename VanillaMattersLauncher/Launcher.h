3670dc17c638a01be72ed28db7afce270fd7972a
TheMarkie <themarkie23@gmail.com>
2018-06-20 08:51:54 +0700

    removed all launcher related files
    everything launcher related now has its own repo

--- VanillaMattersLauncher/Launcher.h
+++
@@ -1,9 +0,0 @@
-#pragma warning( disable : 4201 )
-#define STRICT
-#include <windows.h>
-#include <commctrl.h>
-#include <shlobj.h>
-#include "Engine.h"
-#include "UnRender.h"
-#include "Window.h"
-#include "resource.h"
